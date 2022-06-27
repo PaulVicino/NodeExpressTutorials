e1a2d7c47ccad0c40e8407e0c62fcec2e8f4ee48
PaulVicino <p.vicino715@gmail.com>
2022-06-27 18:01:11 -0400

    node tutorial finished

--- 1-intro.js
+++ 1-intro.js
@@ -4,3 +4,5 @@ if (amount < 10)
     console.log("Small number")
 else
     console.log("Big number")
+
+console.log("Hello World")
--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const { createReadStream } = require('fs');
+var http = require('http')
+var fs = require('fs')
 
-const stream = createReadStream('./content/big.txt');
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
 
-stream.on('data', (result) => {
-    console.log(result);
-})
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
